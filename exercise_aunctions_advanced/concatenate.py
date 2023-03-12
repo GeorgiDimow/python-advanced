7fe6f18427812c7ea8de994df6347371e714b6b0
GeorgiDimow <114015702+GeorgiDimow@users.noreply.github.com>
2023-03-12 21:48:39 +0200

    Delete exercise_aunctions_advanced directory

--- exercise_aunctions_advanced/concatenate.py
+++
@@ -1,7 +0,0 @@
-def concatenate(*args, **kwargs):
-    text = ''.join(args)
-
-    for key in kwargs:
-        text = text.replace(key, kwargs[key])
-
-    return text
--- exercise_aunctions_advanced/function_executor.py
+++
@@ -1,2 +0,0 @@
-def func_executor(*func_data):
-    return '\n'.join([f"{func.__name__} - {func(*args)}" for func, args in func_data])
